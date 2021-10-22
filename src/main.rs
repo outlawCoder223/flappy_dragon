78c23880b224f2fb490c5e0757875a5a6aa5977f
Rance <rance@outlawcoding.com>
2021-10-22 06:51:45 -0600

    implement player

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,53 @@ enum GameMode {
     End,
 }
 
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Self {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 struct State {
     mode: GameMode,
+    player: Player,
+    frame_time: f32,
 }
 
 impl State {
     fn new() -> Self {
         Self {
             mode: GameMode::Menu,
+            player: Player::new(5, 25),
+            frame_time: 0.0,
         }
     }
 
@@ -25,6 +64,8 @@ impl State {
     fn restart(&mut self) {
         // TODO: Fill in stub
         self.mode = GameMode::Playing;
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
